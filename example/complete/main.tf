e715ab8b2c752e370aa50ae5d2492c367c5f5a56
Greg <gregeast94@gmail.com>
2025-06-24 21:03:49 -0400

    feat(terraform-aws-networking-tf-course): add files

--- example/complete/main.tf
+++ example/complete/main.tf
@@ -1,15 +1,8 @@
-# terraform-aws-networking-tf-course
-
-This module manages the creation of VPCs and Subnets, allowing for the creation of both private and public subnets.
-
-Example usages:
-
-```
 module "vpc" {
   source = "./modules/networking"
   vpc_config = {
     cidr_block = "10.0.0.0/16"
-    name       = "your_vpc"
+    name       = "13-local-modules"
   }
   subnet_config = {
     subnet_1 = {
@@ -19,9 +12,9 @@ module "vpc" {
 
     subnet_2 = {
       cidr_block = "10.0.1.0/24"
-      public     = true
-      az         = "us-east-1b"
+      # Public subnets are indicated by setting the "public" option to true.
+      public = true
+      az     = "us-east-1b"
     }
   }
 }
-```
